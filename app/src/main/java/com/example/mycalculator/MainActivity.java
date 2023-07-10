78b310bd12bb739d9e7e528ccbe124d64ed7af45
Anirudh Parvatikar <71212295+anirudhp06@users.noreply.github.com>
2023-07-10 20:22:06 +0530

    New approach to calculate Reduce amount of java code by implementing switch-case approach.

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -14,6 +14,48 @@ public class MainActivity extends AppCompatActivity {
     private EditText opr1,opr2;
     private Button btnadd,btnsub,btndiv,btnmul,clear;
     private TextView txtresult;
+
+
+    public void cal(EditText opr1,EditText opr2,String ops) {
+        Double oper1,oper2;
+        try {
+            oper1 = Double.parseDouble(opr1.getText().toString());
+            oper2 = Double.parseDouble(opr2.getText().toString());
+        }
+        catch (NumberFormatException n){
+            /*
+            * App eventually crashes without try-catch or dosen't respond to anything as Double can't parse empty values.
+            * Need to use try-catch block because if user passes no values, displaying toast is not possible
+            * setting 2 operators to some default value to detect if user hasn't passed any values.
+            * */
+            oper1=-5555555.00;
+            oper2=-5555555.00;
+        }
+        Double result;
+        if(oper1!=-5555555.00 && oper2!=-5555555.00){
+            switch (ops) {
+                case "+":
+                    result = oper1 + oper2;
+                    txtresult.setText(Double.toString(result));
+                    break;
+                case "-":
+                    result = oper1 - oper2;
+                    txtresult.setText(Double.toString(result));
+                case "*":
+                    result = oper1 * oper2;
+                    txtresult.setText(Double.toString(result));
+                case "/":
+                    result = oper1 / oper2;
+                    txtresult.setText(Double.toString(result));
+            }
+        }
+        else{
+            Toast toast=Toast.makeText(MainActivity.this,"Please enter all fields",Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,62 +72,25 @@ public class MainActivity extends AppCompatActivity {
         btnadd.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                if(opr1.getText().length()>0 && opr2.getText().length()>0){
-                    double oper1=Double.parseDouble(opr1.getText().toString());
-                    double oper2=Double.parseDouble(opr2.getText().toString());
-                    double result=oper1+oper2;
-                    txtresult.setText(Double.toString(result));
-                }
-                else{
-                    Toast toast= Toast.makeText(MainActivity.this,"Please enter all fields",Toast.LENGTH_LONG);
-                    toast.show();
-                }
+                cal(opr1,opr2,"+");
             }
         });
         btnsub.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                if(opr1.getText().length()>0 && opr2.getText().length()>0){
-                    double oper1=Double.parseDouble(opr1.getText().toString());
-                    double oper2=Double.parseDouble(opr2.getText().toString());
-                    double result=oper1-oper2;
-                    txtresult.setText(Double.toString(result));
-                }
-                else{
-                    Toast toast= Toast.makeText(MainActivity.this,"Please enter all fields",Toast.LENGTH_LONG);
-                    toast.show();
-                }
+                cal(opr1,opr2,"-");
             }
         });
         btnmul.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                if(opr1.getText().length()>0 && opr2.getText().length()>0){
-                    double oper1=Double.parseDouble(opr1.getText().toString());
-                    double oper2=Double.parseDouble(opr2.getText().toString());
-                    double result=oper1*oper2;
-                    txtresult.setText(Double.toString(result));
-                }
-                else{
-                    Toast toast= Toast.makeText(MainActivity.this,"Please enter all fields",Toast.LENGTH_LONG);
-                    toast.show();
-                }
+                cal(opr1,opr2,"*");
             }
         });
         btndiv.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                if(opr1.getText().length()>0 && opr2.getText().length()>0){
-                    double oper1=Double.parseDouble(opr1.getText().toString());
-                    double oper2=Double.parseDouble(opr2.getText().toString());
-                    double result=oper1/oper2;
-                    txtresult.setText(Double.toString(result));
-                    Log.w("Div","Done with Division of "+Double.toString(oper1)+","+Double.toString(oper2));
-                }
-                else{
-                    Toast toast= Toast.makeText(MainActivity.this,"Please enter all fields",Toast.LENGTH_LONG);
-                    toast.show();
-                }
+                cal(opr1,opr2,"/");
             }
         });
         clear.setOnClickListener(new View.OnClickListener(){
